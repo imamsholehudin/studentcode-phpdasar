9887cc872babb3415b76577e834410c56ab55adb
imamsholehudin <sholehudinimam@gmail.com>
2020-05-07 16:39:05 +0700

    video ke-12
    belajar insert data dan hapus data menggunakan php dasar

---
+++ pertemuan10/function.php
@@ -0,0 +1,63 @@
+<?php
+
+ $server = "localhost"; //nama server di local hostnya
+  $user = "root"; // nama usernya
+  $password = ""; // pasword servernya defaultnya kosong
+  $db = "database_latihan"; // nama databsenya
+
+  
+  $conn = mysqli_connect($server, $user, $password, $db);
+
+function query($query){
+  global $conn;
+  $query="select * from mahasiswa";
+
+  $result = mysqli_query($conn, $query);
+
+
+  $rows = [];
+
+
+  while($row = mysqli_fetch_assoc($result)){
+   
+    $rows [] = $row;
+
+  }
+
+  return $rows;
+
+
+}
+
+function tambah($data){
+  global $conn;
+  //htmlspecialchars di gunakan untuk menangkis
+  //hacker yang mencoba memasukan script HTML kedalam form
+  //lihat contoh pada tabel mahasiswa 014
+  $nrp = htmlspecialchars($data["nrp"]);
+  $nama = htmlspecialchars($data["nama"]);
+  $email = htmlspecialchars($data["email"]);
+  $jurusan = htmlspecialchars($data["jurusan"]);
+  $gambar = htmlspecialchars($data["gambar"]);
+  
+
+  $query = "INSERT INTO mahasiswa VALUES('','$nama','$nrp','$email','$jurusan','$gambar')";
+  mysqli_query($conn,$query);
+  var_dump(mysqli_query($conn,$query));
+  
+
+  return mysqli_affected_rows($conn);
+}
+
+function hapus($id){
+  global $conn;
+  mysqli_query($conn, "DELETE FROM mahasiswa WHERE id=$id");
+  return mysqli_affected_rows($conn);
+
+}
+
+
+
+
+
+ ?>
