37af661f5b1191038a86774e81ba6fc1b8433a36
imamsholehudin <sholehudinimam@gmail.com>
2020-05-08 13:36:15 +0700

    video ke-17
    nambahin fitur login

---
+++ pertemuan15/index.php
@@ -0,0 +1,70 @@
+<?php
+//kita me konekan ke file functions.php
+$data =null;
+require "function.php";
+//query disini merupakan FUNCTION yang dibuat di functions.php
+//$mahasiswa menjadi FUNCTION QUERY yang berisi SYNTAX mysql
+$mahasiswa = query("SELECT * FROM mahasiswa ORDER BY id DESC");
+
+
+if (isset($_POST["cari"])){
+
+  $mahasiswa = cari($_POST['keyword']);
+   var_dump($_POST['keyword']);
+   echo "<br>";
+ 
+}
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Halaman Admin</title>
+  </head>
+  <body>
+    <h1>daftar mahasiswa</h1>
+    <a href="tambah.php">Tambah daftar Mahasiswa</a>
+    <br><br>
+  </body>
+</html>
+<form class="" action="" method="post">
+  <input type="text" name="keyword" size="30" autofocus
+  placeholder="masukan keyword pencarian" autocomplete="off">
+  <button type="submit" name="cari">cari data</button><br><br>
+
+</form>
+<table border='1' cellpadding='10' cellspacing='0'>
+  <tr>
+    <th>no.</th>
+    <th>aksi</th>
+    <th>gambar</th>
+    <th>nrp</th>
+    <th>nama</th>
+    <th>email</th>
+    <th>jurusan</th>
+  </tr>
+  <?php $i = 1;?>
+  <?php foreach ($mahasiswa as $row) : ?>
+  <tr>
+    <td><?php echo $i; ?></td> <!-- ini urutan untuk nomer -->
+    <td>
+      <a href="ubah.php?id=<?php echo $row['id']; ?>">ubah</a> | <!-- ini urutan untuk aksi -->
+      <!-- href kita arahkan ke link hapus.php untuk menghapus
+      sesuai id-->
+      <!-- onclick returt confirm merupakan script java script
+      untuk menghasilkan alert komfirmasi-->
+      <a href="hapus.php?id=<?php echo $row['id']; ?>" onclick="return confirm('yakin ?');">hapus</a>
+    </td>
+    <td>
+      <img src="<?php echo $row['gambar']; ?>" alt="" width='50px'><!-- ini urutan untuk gambar -->
+    </td>
+    <td><?php echo $row['nrp']; ?></td>
+    <td><?php echo $row['nama']; ?></td>
+    <td><?php echo $row['email']; ?></td>
+    <td><?php echo $row['jurusan']; ?></td>
+    <?php $i++;?>
+  </tr>
+
+<?php endforeach; ?>
+</table>
---
+++ pertemuan15/login.php
@@ -0,0 +1,85 @@
+<?php
+  require "function.php";
+
+  //login system dimulai disini
+  if (isset($_POST['login'])) {
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+
+      //cek apakah benar ada username yang diinput di didalam database..
+      $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+      //bila ingin tau fungsi num_rows silakan var_dump($result)
+      // var_dump($result); die;
+      //apabila ada username yang sama maka num_rows akan menghasilkan 1
+
+      if (mysqli_num_rows($result) === 1) {
+
+        //sekarang kita cek apakah password ada..?
+        //buat variable penampung semua data dari $username
+        //$row disini akan mengatur isi dari $result sebagai assosiasi / assoc
+          $row = mysqli_fetch_assoc($result);
+
+        //selanjutnya kita decrypt password mengunakan password_verify()
+        //password_verify memiliki 2 parameter,
+        //1. input password yang akan di cek 2. password yang sudah di encrypsi
+        if(password_verify($password, $row['password'])){
+
+          //jika pasword sudah terverifikasi arahkan user ke halaman yang diinginkan
+          // biasanya menggunakan >>> echo "<script>document.location.href='index.php'</script>";;
+          //kita belajar menggunakan header()
+          //Location L nya huruf besar..
+          header("Location:index.php");
+          //agar berhenti pada header, gunakan exit
+          exit;
+        }
+      }
+
+      //jika proses sampai disini, berarti username dan password salah,
+      //karena tidak nyangkut di function atas..
+      //kita buat variable baru agar bisa mengatur
+      //pesan di halaman htmlnya
+      $error = true;
+      //fungsi eror akan menyalah..
+  }
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>login</title>
+   </head>
+   <body>
+     <center>
+     <h3 style="text-align:center">login mahasiswa</h3>
+
+     <!-- PESAN ERROR KITA BERIKAN DISINI
+      apabila ada $error masuk ke halaman ini...
+   -->
+     <?php if (isset($error)) : ?>
+       <img src="img/fadli.jpg" alt="">
+       <p style="color : red; font-style:italic">username / password salah</p>
+     <?php endif;  ?>
+     <!-- PESAN ERROR KITA BERIKAN DISINI-->
+
+     <form  action="" method="post">
+       <ul style="list-style-type:none">
+         <li>
+           <input type="text" name="username" placeholder="masukan username">
+         </li>
+         <li>
+           <input type="password" name="password" placeholder="masukan password">
+         </li>
+         <li>
+           <button type="submit" name="login">login</button>
+         </li>
+       </ul>
+     </form>
+   </center>
+
+
+
+
+   </body>
+ </html>
